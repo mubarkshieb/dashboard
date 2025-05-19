c41f0a728c571739c8e05f2a9383c20a3c1c0deb
mubarkshieb <mubark_96@yahoo.com>
2025-05-19 11:32:23 +0300

    side bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import styles from "./Navbar.module.css";
+import { User } from 'lucide-react';
 const Navbar = () => {
     return (
         <div className={styles.container}>
             <div className={styles.logo}><text>SMART</text><span>SOLUTIONS.</span></div>
+            <User />
         </div>
     )
 }
--- src/Components/Sidebar/SideBar.jsx
+++ src/Components/Sidebar/SideBar.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import styles from './Sidebar.module.css';
+import { House, Settings, Search } from 'lucide-react';
 
 const SideBar = () => {
     return (
-        <div className={styles.container}>SideBar</div>
+        <div className={styles.container}>
+            <div className={styles.icons}>
+                <div className={styles.house}>
+                    <House />
+                </div>
+                <div className={styles.search}>
+                    <Search />
+                </div>
+                <div className={styles.settings}>
+                    <Settings />
+                </div>
+            </div>
+        </div>
     )
 }
 
